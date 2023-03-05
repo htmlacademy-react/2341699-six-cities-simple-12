58b5477c1342c348f3455e487472189042ab0385
ShadrovSV <grayhb@mail.ru>
2023-03-05 13:00:49 +0400

    Добавил основные страницы и компонент (place-card) для отображения объявления на main-page.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,11 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/main-page';
+
+type AppProps = {
+  placesFound: number;
+};
+
+function App(props: AppProps): JSX.Element {
+  return <MainPage placesFound={props.placesFound} />;
 }
 
 export default App;
