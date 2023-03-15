e9088aebe53d9e1737695f5e68dfc80a73e3c804
ShadrovSV <grayhb@mail.ru>
2023-03-15 15:27:26 +0400

    Вернул типы для исправления ошибок линтера в github

---
+++ project/src/types/offer.ts
@@ -0,0 +1,40 @@
+type Offer = {
+  id: number;
+
+  /*
+    bedrooms: number
+    maxAdults: number;
+
+    description: string;
+    goods: string[];
+    host: {
+      avatarUrl: string;
+      id: number;
+      isPro: boolean;
+      name: string;
+    };
+    images: string[];
+  */
+  city: {
+    location: {
+      latitude: number;
+      longitude: number;
+      zoom: number;
+    };
+    name: string;
+  };
+  isPremium: boolean;
+  previewImage: string;
+  price: number;
+  rating: number;
+  title: string;
+  type: string;
+
+  location: {
+    latitude: number;
+    longitude: number;
+    zoom: number;
+  };
+};
+
+export default Offer;
