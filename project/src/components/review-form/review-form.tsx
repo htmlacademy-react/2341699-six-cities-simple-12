0d8eaca8a8ea5c335fcc516ab48438b4d4822be7
ShadrovSV <grayhb@mail.ru>
2023-03-16 11:07:08 +0400

    Рефакторинг формы ввода комментария

--- project/src/components/review-form/review-form.tsx
+++ project/src/components/review-form/review-form.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 
 function ReviewForm(): JSX.Element {
 
@@ -14,45 +14,21 @@ function ReviewForm(): JSX.Element {
     e.preventDefault();
   };
 
+  const ratingArray = [5, 4, 3, 2, 1];
+
   return (
     <form className="reviews__form form" action="#" method="post" onSubmit={handleFormSubmit}>
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
-
-        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" onChange={() => setRating(5)} />
-        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" onChange={() => setRating(4)} />
-        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" onChange={() => setRating(3)} />
-        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" onChange={() => setRating(2)} />
-        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" onChange={() => setRating(1)} />
-        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly" >
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
+        {ratingArray.map((r) => (
+          <Fragment key={r}>
+            <input className="form__rating-input visually-hidden" name="rating" value={r} id={`${r}-stars`} type="radio" onChange={() => setRating(r)} />
+            <label htmlFor={`${r}-stars`} className="reviews__rating-label form__rating-label" title="perfect">
+              <svg className="form__star-image" width="37" height="33">
+                <use xlinkHref="#icon-star"></use>
+              </svg>
+            </label>
+          </Fragment>))}
       </div>
       <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" value={comment} onChange={handleChangeComment}>
       </textarea>
